dad573c78cae12c8e9be7cf7e786ce4ea83b5edb
Matinmmp1381 <matinmmp1381@gmail.com>
2025-02-14 14:08:33 +0330

    fix deploy problem

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import { colors, nextui } from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default  {
     content: [
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
