3c6965d50400dd5b96764d8a2dc7d6854ac774fb
Matinmmp1381 <matinmmp1381@gmail.com>
2025-04-08 23:27:48 +0430

    blog

--- src/app/blog/blogs/[blogName]/page.tsx
+++ src/app/blog/blogs/[blogName]/page.tsx
@@ -20,80 +20,79 @@ type Props = {
 };
 
 export default async function BlogDetail({ params: { blogName } }: Props) {
-    
-    const data:any = await getBlogBySlug(blogName)
-    console.log(data)
+
+    const data: any = await getBlogBySlug(blogName)
+
 
     if (data && data?.message === "بلاگ مورد نظر یافت نشد")
         notFound();
 
     if (data && data.success) {
-    const blog = data?.blog;
-    const schema: any = {
-        "@context": "https://schema.org",
-        "@type": "Course",
-        "name": data?.courseData?.course?.seoMeta?.title || data?.courseData?.course?.name,
-        "description": data?.courseData?.course?.seoMeta?.description,
-        "provider": {
-            "@type": "Organization",
-            "name": "Virtual Learn",
-            "url": "https://www.vc-virtual-learn.com"
-        },
-        "isPartOf": {
-            "@type": "WebSite",
-            "name": "ویرچوال لرن",
-            "url": "https://www.vc-virtual-learn.com"
-        },
-        "hasCourseInstance": {
-            "@type": "CourseInstance",
-            "name": data?.courseData?.course?.name,
-            "courseMode": "Online",
-            "startDate": data?.courseData?.course?.createDate,
-            "endDate": data?.courseData?.course?.endDate,
-            "courseWorkload": "PT2H", // 2 ساعت در هفته به فرمت ISO 8601
-            "courseSchedule": "Flexible", // برای جلوگیری از خطا اضافه شده
-       
+        const blog = data?.blog;
 
-            "aggregateRating":{
-                "@type": "AggregateRating",
-                "ratingValue": 4.7,
-                "reviewCount": 134,
-                "bestRating": "5"
-            }
-        },
-    };
+        const schema = {
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            "mainEntityOfPage": {
+                "@type": "WebPage",
+                "@id": `https://www.vc-virtual-learn.com/blog/blogs/${blogName}`
+            },
+            "name": `${blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name}`,
+            "headline": `${blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name}`,
+            "description": `${data?.courseData?.course?.seoMeta?.description}`,
+            "image": blog?.thumbnail?.imageUrl,
+            "author": {
+                "@type": "Organization",
+                "name": "Virtual Learn",
+                "url": "https://www.vc-virtual-learn.com"
+            },
+            "publisher": {
+                "@type": "Organization",
+                "name": "Virtual Learn",
+                "logo": {
+                    "@type": "ImageObject",
+                    "url": `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}logo-main.png`,
+                    "width": 512,
+                    "height": 512
+                }
+            },
+            "datePublished": `${blog?.createdAt}`,
+            "inLanguage": "fa"
+        }
 
-    return (
-        <>
-            <section className=" flex flex-col items-center justify-center" >
-                <meta property="og:image" content={blog?.thumbnail?.imageUrl} />
-                <title>{blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name}</title>
-                <meta name="description" content={blog?.seoMeta?.description} />
-                <meta name="keywords" content={blog?.seoMeta?.keywords} />
-                <meta name="robots" content="index, follow" />
-                <meta name="revisit-after" content="5 days" />
 
-                <meta property="og:title" content={blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name} />
-                <meta property="og:description" content={blog?.seoMeta?.description} />
-                <meta property="og:url" content={`https://www.vc-virtual-learn.com/courses/${blogName}`} />
-                <meta property="og:image" content={blog?.thumbnail?.imageUrl} />
+        return (
+            <>
+                <section className=" flex flex-col items-center justify-center" >
 
-                <meta name="twitter:title" content={blog?.seoMeta?.title ? blog?.seoMeta?.title : data?.blog?.name} />
-                <meta name="twitter:description" content={blog?.seoMeta?.description} />
+                    <title>{blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name}</title>
+                    <meta name="description" content={blog?.seoMeta?.description} />
+                    <meta name="keywords" content={blog?.seoMeta?.keywords} />
+                    <meta name="robots" content="index, follow" />
+                    <meta name="revisit-after" content="5 days" />
 
-                {/* بعدا عکسو بذار */}
-                <meta name="twitter:image" content={blog?.thumbnail?.imageUrl} />
+                    <meta property="og:title" content={blog?.seoMeta?.title ? blog?.seoMeta?.title : blog?.name} />
+                    <meta property="og:description" content={blog?.seoMeta?.description} />
+                    <meta property="og:url" content={`https://www.vc-virtual-learn.com/courses/${blogName}`} />
+                    <meta property="og:image" content={blog?.thumbnail?.imageUrl} />
 
-                <link rel="canonical" href={`https://www.vc-virtual-learn.com/courses/${blogName}`} />
 
-                <BlogInfo data={blog} />
+                    <meta name="twitter:title" content={blog?.seoMeta?.title ? blog?.seoMeta?.title : data?.blog?.name} />
+                    <meta name="twitter:description" content={blog?.seoMeta?.description} />
+                    <meta name="twitter:image" content={blog?.thumbnail?.imageUrl} />
 
-                {/* <Script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} /> */}
+                    <link rel="canonical" href={`https://www.vc-virtual-learn.com/courses/${blogName}`} />
 
-            </section>
-            <div className="gap-10 relative"><span className=" dark:bg-[rgba(21,130,255,0.10)] absolute  right-1 w-10 h-10 background -z-30" /></div>
-        </>
-    );
-    // }
-}
+
+
+                    <BlogInfo data={blog} />
+
+                    <Script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
+
+                </section>
+                <div className="gap-10 relative"><span className=" dark:bg-[rgba(21,130,255,0.10)] absolute  right-1 w-10 h-10 background -z-30" /></div>
+            </>
+        );
+        // }
+    }
 }
